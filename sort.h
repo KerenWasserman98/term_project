9da17d92373a8cce6def8ad3437d4387c20b63cc
KerenWasserman98 <101370769+KerenWasserman98@users.noreply.github.com>
2022-03-10 15:59:06 -0800

    Update sort.h

--- sort.h
+++ sort.h
@@ -17,5 +17,54 @@ void partition(int *arr, const int left, const int right);
 void heap_sort(int *arr, int n); 
 //add any other helper funcitons
 
-void counting_sort(int *A, int *B, int k, int n); 
-void radix_sort(int *A, int *B, int n, int d); 
+
+//radix and counting sort
+int getMaxVal(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+void countSort(int arr[], int n) {
+    int maxVal=getMaxVal(arr, n);
+    int *auxArr = new int[maxVal + 1];
+    for (int i = 0; i < maxVal + 1; i++) 
+        auxArr[i] = 0;
+    for (int j = 0; j < n; j++) //count
+        auxArr[arr[j]]++;
+    for (int k = 1; k < maxVal + 1; k++) { //sum
+        auxArr[k] = auxArr[k] + auxArr[k - 1];
+    }
+    int* outputArr = new int[n];
+    for (int i = n-1; i >=0; i--) {
+        int val = arr[i];
+        outputArr[auxArr[val]-1] = val;
+        auxArr[val]=--auxArr[val];
+    }
+    for (int i = 0; i < n; i++) {
+        arr[i] = outputArr[i];
+    }
+}
+void radixSort(int arr[], int n) {
+    int maxVal = getMaxVal(arr, n);
+    for (int div = 1; maxVal / div > 0; div *= 10) {
+        int auxArr[10];
+        for (int i = 0; i < 10; i++) 
+            auxArr[i] = 0;
+        for (int j = 0; j < n; j++)
+            auxArr[(arr[j] / div) % 10]++;
+        for (int k = 1; k < 10; k++) 
+            auxArr[k] = auxArr[k] + auxArr[k - 1];
+        int *outputArr = new int[n];
+        for (int i = n-1; i >= 0; i--) {
+            int val = arr[i];
+            outputArr[auxArr[(val / div) % 10] - 1] = val;
+            auxArr[(val / div) % 10]--;
+        }
+        for (int i = 0; i < n; i++)
+            arr[i] = outputArr[i];
+    }
+}
